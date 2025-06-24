e6000f0ff9108b0f8967a52b2ef8a62dc0ea9d3a
TANJIM AKTER ANISA PAYEL <119970143+anisa-payel@users.noreply.github.com>
2025-06-24 11:05:05 +0600

    app.py

--- app.py
+++ app.py
@@ -9,41 +9,43 @@ le_model = joblib.load('Src/label_encoder.pkl')
 
 st.title("🌦️ Weather Prediction App")
 st.header("Predict Weather!!")
-
 st.write("We,ve made a weather predicting app which using Deep Learning/*")
-
 st.sidebar.header("Write the inputs")
 
-def user_input_features():
-    temp = st.sidebar.slider('Enter Today,s Temperature(C)',15.0, 40.0, 25.0)
-    tempmin = st.sidebar.slider('Enter Today,s Minimum Temperature(C)',10.0, 30.0, 17.0)
-    tempmax = st.sidebar.slider('Enter Today,s Maximum Temperature(C)',17.0, 40.0, 30.0)
-    feelslike = st.sidebar.slider('Enter The Feellike Temperature(C)',25.0, 45.0, 32.0)
-    feelslikemin = st.sidebar.slider('Enter The Feellike Min Temperature(C)',25.0, 45.0, 32.0)
-    feelslikemax = st.sidebar.slider('Enter The Feellike Max Temperature(C)',18.0, 55.0, 34.0)
-    dew = st.sidebar.slider('Enter The Dew',8.0, 28.0, 20.0)
-    precipprob = st.number_input('Enter Precipitation Probability(0-100)',0, 100, 66)
-    precipcover = st.sidebar.slider('Enter Precipitation Coverage',0, 100.0, 17)
-    cloudcover = st.sidebar.slider('Enter The CloudCover',0.0, 100.0,56.0)
-    humidity = st.sidebar.slider('Enter The Humidity',51.0, 98.0,82.0)
-    conditions = st.selectbox( 'Select The Weather Condition of Today', options=["Rain, Overcast", "Rain, Partially cloudy", "Partially cloudy","Clear"])
 
+temp = st.sidebar.slider('Enter Today,s Temperature(C)',15.0, 40.0, 25.0)
+tempmin = st.sidebar.slider('Enter Today,s Minimum Temperature(C)',10.0, 30.0, 17.0)
+tempmax = st.sidebar.slider('Enter Today,s Maximum Temperature(C)',17.0, 40.0, 30.0)
+feelslike = st.sidebar.slider('Enter The Feellike Temperature(C)',25.0, 45.0, 32.0)
+feelslikemin = st.sidebar.slider('Enter The Feellike Min Temperature(C)',25.0, 45.0, 32.0)
+feelslikemax = st.sidebar.slider('Enter The Feellike Max Temperature(C)',18.0, 55.0, 34.0)
+dew = st.sidebar.slider('Enter The Dew',8.0, 28.0, 20.0)
+precipprob = st.number_input('Enter Precipitation Probability(0-100)',0, 100, 66)
+precipcover = st.sidebar.slider('Enter Precipitation Coverage',0, 100.0, 17)
+cloudcover = st.sidebar.slider('Enter The CloudCover',0.0, 100.0,56.0)
+humidity = st.sidebar.slider('Enter The Humidity',51.0, 98.0,82.0)
+conditions = st.selectbox( 'Select The Weather Condition of Today', options=["Rain, Overcast", "Rain, Partially cloudy", "Partially cloudy","Clear"])
+
+predict = st.button(label = "Predict tommorrows Weather")
+
+if predict: 
     data = {
-        'temp': temp,
-        'tempmin' : tempmin,
-        'tempmax': tempmax,
-        'feelslike': feelslike,
-        'feelslikemin': feelslikemin,
-        'feelslikemax': feelslikemax,
-        'dew' : dew,
-        'precipprob':  precipprob,
-        'precipcover': precipcover,
-        'cloudcover': cloudcover,
-        'humidity': humidity,
-        'conditions': conditions}
+            'temp': temp,
+            'tempmin' : tempmin,
+            'tempmax': tempmax,
+            'feelslike': feelslike,
+            'feelslikemin': feelslikemin,
+            'feelslikemax': feelslikemax,
+            'dew' : dew,
+            'precipprob':  precipprob,
+            'precipcover': precipcover,
+            'cloudcover': cloudcover,
+            'humidity': humidity,
+            'conditions': conditions
+    }
 
     features =  pd.DataFrame(data, index=[0])
-return features
+    return features
 
 df = user_input_features
 predictions = temp_model.predict(df)
